bfcc23fbfc28095a56761e9e3c9c03bc0a8642cd
AdrienNguyen <phuong.na163228@gmail.com>
2021-12-25 19:30:14 +0700

    fix custom translate and add empty file

--- src/layouts/footer/client-footer.tsx
+++ src/layouts/footer/client-footer.tsx
@@ -1,7 +1,8 @@
+import { t } from '@/locales'
 import React from 'react'
 
 const ClientFooter: React.FC = () => {
-    return <footer className="client-footer">This is client footer</footer>
+    return <footer className="client-footer">{t('FOOTER')}</footer>
 }
 
 export default ClientFooter
--- src/layouts/header/client-header.tsx
+++ src/layouts/header/client-header.tsx
@@ -1,16 +1,20 @@
 import { t } from '@/locales'
 import { useUpdateLocale } from '@/locales/context'
-import React, { ChangeEvent } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import { Link } from 'wouter'
 
 const ChooseLanguage = () => {
+    const [locale, setLocale] = useState(localStorage.lang || 'en')
+
     const updateLocale = useUpdateLocale()
     const handleSelect = (e: ChangeEvent<HTMLSelectElement>) => {
-        updateLocale(e.target.value)
+        const locale = e.target.value
+        setLocale(locale)
+        updateLocale(locale)
     }
     return (
         <>
-            <select onChange={handleSelect}>
+            <select onChange={handleSelect} value={locale}>
                 <option value="en">English</option>
                 <option value="vi">Vietnamese</option>
             </select>
